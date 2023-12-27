9e3cc2aad907039f3b891bf75e18b44d6f280aaf
Joshua Moreira <joshuamoreira3@gmail.com>
2023-12-27 16:21:37 -0600

    Updated build process to support GitHub Pages

--- builder-app/next.config.js
+++ builder-app/next.config.js
@@ -1,6 +1,6 @@
 const withBuilderDevTools = require("@builder.io/dev-tools/next")();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withBuilderDevTools({});
+const nextConfig = withBuilderDevTools({ distDir: "build" });
 
 module.exports = nextConfig;
